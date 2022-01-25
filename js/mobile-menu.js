53471983a3950251d130b61bd61536c3c2882baf
Ohleh <oleg.kolischuk@gmail.com>
2022-01-25 20:29:26 +0200

    +1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,23 +1,3 @@
-// (() => {
-//     const menuBtnRef = document.querySelector("[data-menu-button]");
-//     // const mobileMenuRef = document.querySelector("[data-menu-container]");
-//     // const body = document.getElementsByTagName("body")[0];
-//     menuBtnRef.addEventListener("click", () => {
-//         // body.classList.toggle("modal-open");
-//         // const expanded =
-//         //     menuBtnRef.getAttribute("aria-expanded") === "true" || "false";
-    
-//         menuBtnRef.classList.toggle("is-open");
-//         // menuBtnRef.setAttribute("aria-expanded", !expanded);
-//         // mobileMenuRef.classList.toggle("is-opened");
-    
-    
-    
-//      });
-// })();
-//
-//
-//
     (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
