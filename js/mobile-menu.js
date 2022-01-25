62372a30a5fcbbeb9f95b4a4f6d7a42025e2f3aa
Ohleh <oleg.kolischuk@gmail.com>
2022-01-25 19:19:16 +0200

    makimg burger

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,20 +1,23 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-burger-btn]");
-    const mobileMenuRef = document.querySelector("[data-menu-container]");
-    const body = document.getElementsByTagName("body")[0];
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    // const mobileMenuRef = document.querySelector("[data-menu-container]");
+    // const body = document.getElementsByTagName("body")[0];
     menuBtnRef.addEventListener("click", () => {
-        body.classList.toggle("modal-open");
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || "false";
+        // body.classList.toggle("modal-open");
+        // const expanded =
+        //     menuBtnRef.getAttribute("aria-expanded") === "true" || "false";
     
-        menuBtnRef.classList.toggle("is-opened");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
-        mobileMenuRef.classList.toggle("is-opened");
+        menuBtnRef.classList.toggle("is-open");
+        // menuBtnRef.setAttribute("aria-expanded", !expanded);
+        // mobileMenuRef.classList.toggle("is-opened");
     
     
     
      });
 })();
+//
+//
+//
 //     (() => {
 //   const menuBtnRef = document.querySelector("[data-menu-button]");
 //   const mobileMenuRef = document.querySelector("[data-menu]");
