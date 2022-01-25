60e3eee184ee5154a876f0fd95f7fe40aa6f8273
Ohleh <oleg.kolischuk@gmail.com>
2022-01-25 20:24:39 +0200

    +1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -29,3 +29,21 @@
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+
+
+// (() => {
+//   const refs = {
+//     menuBtn: document.querySelector("[data-menu-button]"),
+//     mobileMenu: document.querySelector("[data-menu]"),
+//     body: document.querySelector("body"),
+//   };
+//   refs.menuBtn.addEventListener("click", () => {
+//     const expanded =
+//       refs.menuBtn.getAttribute("aria-expanded") === "true" || false;
+//     refs.menuBtn.setAttribute("aria-expanded", !expanded);
+//     refs.menuBtn.classList.toggle("is-open");
+//     refs.mobileMenu.classList.toggle("is-open");
+//     refs.body.classList.toggle("is-open");
+//   });
+// })();
